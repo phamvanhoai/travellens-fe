e4a153f81512574e5f06fb9fb5a270a0d7f5f56a
Lethinh04 <ThinhLCE180136@fpt.edu.vn>
2026-06-06 21:05:54 +0700

    complete forgot password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,4 +5,7 @@ export const authService = {
   register: (payload: { name: string; email: string; password: string; confirm_password?: string }) => api.post("/auth/register", payload),
   verifyEmail: (payload: { email: string; otp: string }) => api.post("/auth/verify-email", payload),
   googleLogin: (payload: { id_token: string }) => api.post("/auth/google", payload),
+  forgotPassword: (payload: { email: string }) => api.post("/auth/forgot-password", payload),
+  verifyResetCode: (payload: { email: string; code: string }) => api.post("/auth/verify-reset-code", payload),
+  resetPassword: (payload: { reset_token: string; new_password: string }) => api.post("/auth/reset-password", payload),
 };
